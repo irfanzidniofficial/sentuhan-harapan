a50c501652d6c4787215817d41f84eb08568bed8
irfanzidniofficial <irfanzidniofficial@gmail.com>
2024-04-22 22:50:45 +0700

    custom breeze user register and apply fundraiser

--- resources/views/admin/fundraisers/index.blade.php
+++ resources/views/admin/fundraisers/index.blade.php
@@ -9,35 +9,30 @@
     <div>
         
     </div>
+    @role('owner')
     <div class="list-fundraisers py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col gap-y-5">
+
+                @forelse ($fundraisers as $fundraiser)
+                    
+              
+
                 <div class="item-card flex flex-row justify-between items-center">
                     <div class="flex flex-row items-center gap-x-3">
-                        <img src="https://images.unsplash.com/photo-1552196563-55cd4e45efb3?q=80&w=3426&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
+                        <img src="{{ Storage::url($fundraiser->user->avatar) }}" alt="" class="rounded-2xl object-cover w-[90px] h-[90px]">
                         <div class="flex flex-col">
-                            <h3 class="text-indigo-950 text-xl font-bold">Villio Jack</h3>
+                            <h3 class="text-indigo-950 text-xl font-bold">{{ $fundraiser->user->name }}</h3>
                         </div>
                     </div> 
                     <div class="hidden md:flex flex-col">
                         <p class="text-slate-500 text-sm">Date</p>
-                        <h3 class="text-indigo-950 text-xl font-bold">12 Jan 2024</h3>
+                        <h3 class="text-indigo-950 text-xl font-bold">{{ $fundraiser->user->created_at }}</h3>
                     </div>
+                    @if($fundraiser->is_active)
                     <span class="w-fit text-sm font-bold py-2 px-3 rounded-full bg-green-500 text-white">
                         ACTIVE
                     </span>
-                    <span class="w-fit text-sm font-bold py-2 px-3 rounded-full bg-orange-500 text-white">
-                        PENDING
-                    </span>             
-                    <div class="hidden md:flex flex-row items-center gap-x-3">
-                        <form action="#" method="POST">
-                            @csrf
-                            @method('PUT')
-                            <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
-                                Approve
-                            </button>
-                        </form>
-                    </div>
                     <div class="hidden md:flex flex-row items-center gap-x-3">
                         <form action="#" method="POST">
                             @csrf
@@ -47,10 +42,34 @@
                             </button>
                         </form>
                     </div>
+                    @else
+                    <span class="w-fit text-sm font-bold py-2 px-3 rounded-full bg-orange-500 text-white">
+                        PENDING
+                    </span>
+                    <div class="hidden md:flex flex-row items-center gap-x-3">
+                        <form action="{{ route('admin.fundraisers.update', $fundraiser) }}" method="POST">
+                            @csrf
+                            @method('PUT')
+                            <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                                Approve
+                            </button>
+                        </form>
+                    </div>
+
+                    @endif          
+                    
+                   
+                   
                 </div>
+                @empty
+                <p>belum ada apply terbaru</p>
+                    
+                @endforelse
             </div>
         </div>
     </div>
+    @else
+    
     <div class="list-fundraisers py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-10 flex flex-col ">
@@ -67,18 +86,29 @@
                         <h3 class="text-indigo-950 text-xl font-bold">Tebarkan Kebaikan</h3>
                         <p class="text-slate-500 text-base">Jadilah bagian dari kami untuk membantu <br>mereka yang membutuhkan dana tambahan</p>
                     </div>
-                    <form action="#" method="POST">
+
+                    @if($fundraiserStatus=='Pending')
+                    <span class="w-fit text-sm font-bold py-2 px-3 rounded-full bg-orange-500 text-white">
+                        PENDING
+                    </span> 
+                    @elseif($fundraiserStatus=='Active')
+                        <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
+                            Create a Fundraising
+                        </button>
+
+                    @else
+                    <form action="{{ route('admin.fundraiser.apply') }}" method="POST">
                         @csrf
-                        @method('PUT')
+                        
                         <button type="submit" class="font-bold py-4 px-6 bg-indigo-700 text-white rounded-full">
                             Become Fundraiser
                         </button>
                     </form>
-                    <span class="w-fit text-sm font-bold py-2 px-3 rounded-full bg-orange-500 text-white">
-                        PENDING
-                    </span> 
+                    @endif
+                  
                 </div>
             </div>
         </div>
     </div>
+    @endrole
 </x-app-layout>
